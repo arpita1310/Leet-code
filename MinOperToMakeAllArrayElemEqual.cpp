222eb579dd29827e15fd00aee482022baa45b577
Arpita Singh <105579731+arpita1310@users.noreply.github.com>
2023-05-31 23:12:01 +0530

    Add files via upload

---
+++ MinOperToMakeAllArrayElemEqual.cpp
@@ -0,0 +1,23 @@
+#define ll long long 
+class Solution {
+public:
+    vector<long long> minOperations(vector<int>& nums, vector<int>& queries) {
+        vector<ll> v;
+        vector<ll> ps;
+        sort(nums.begin(),nums.end());
+        ll sum = 0,s;
+        for(auto &i: nums){sum += i;ps.push_back(sum);}
+        int x = 0,n=nums.size();
+        for(auto &i: queries){
+            x = upper_bound(nums.begin(),nums.end(),i)-nums.begin();
+            if(x==0){
+                v.push_back((sum-n*1LL*i));
+            } else if(x==n){
+                v.push_back((x*1LL*i - ps[x-1]));
+            }else{
+                v.push_back((x*1LL*i - ps[x-1]) + (sum-ps[x-1]-i*1LL*(n-x)));
+            }
+        }
+        return v;
+    }
+};
