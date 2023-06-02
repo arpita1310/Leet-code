b3aab7ef42fef1b0a37ee53b1b1b9daade6554b5
Arpita Singh <105579731+arpita1310@users.noreply.github.com>
2023-06-02 21:32:47 +0530

    Add files via upload

---
+++ MinTimeDiff.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findMinDifference(vector<string>& time) {
+        vector<int> mi;
+        for(int i=0;i<time.size();i++)
+        {
+            string cur=time[i];
+            int hr=stoi(cur.substr(0,2));
+            int minute=stoi(cur.substr(3,2));
+            int tot=hr*60+minute;
+            mi.push_back(tot);
+        }
+        sort(mi.begin(),mi.end());
+        int mini=INT_MAX;
+        for(int i=0;i<mi.size()-1;i++){
+            int diff=mi[i+1]-mi[i];
+            mini=min(diff,mini);
+        }
+        int last1=(mi[0]+1440)-mi[mi.size()-1];
+        int last2=mi[mi.size()-1]-mi[0];
+        int l=min(last1,last2);
+        mini=min(mini,last1);
+        return mini;
+   }
+};
