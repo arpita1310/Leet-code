19f1e5f4a3ba940d1d9a5cd81c7a5fdb0ba78bd9
Arpita Singh <105579731+arpita1310@users.noreply.github.com>
2023-07-06 22:49:12 +0530

    Add files via upload

---
+++ SingleElementInASortedArray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& n) {
+        unordered_map<int,int>m;
+        for(auto x:n)
+        {
+            m[x]++;
+        }
+        for(auto e:m)
+        {
+            if(e.second==1)
+            {
+                return e.first;
+            }
+        }
+      return 0;   
+    }
+};
