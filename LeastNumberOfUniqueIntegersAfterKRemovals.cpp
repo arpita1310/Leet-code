5f703ee955c418565fee819ce5639ec947fb8acc
Arpita Singh <105579731+arpita1310@users.noreply.github.com>
2023-09-19 06:38:08 +0530

    Add files via upload

---
+++ LeastNumberOfUniqueIntegersAfterKRemovals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<arr.size();i++){
+            mp[arr[i]]++;
+        }
+        int ans;
+        priority_queue<int, vector<int>,greater<int>>pq;
+        for(auto it:mp){
+            pq.push(it.second);
+        }
+        while(k>0){
+            k-=pq.top();
+            if(k>=0){
+                pq.pop();
+            }   
+        }
+        return pq.size();
+    }
+};
