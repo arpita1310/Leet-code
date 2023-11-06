02772f9fac8bb33597cc4f709a1140001a952098
Arpita Singh <105579731+arpita1310@users.noreply.github.com>
2023-11-06 22:02:31 +0530

    Add files via upload

---
+++ BagOfTokens.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        int n = tokens.size();
+        sort(tokens.begin(), tokens.end());
+        int max_score = 0;
+        int curr_score = 0;
+        int i = 0;
+        int j = n - 1;
+        while(i <= j)
+        {
+            if(power >= tokens[i])
+            {
+                curr_score++; 
+                max_score = max(max_score, curr_score);
+                power -= tokens[i];
+                i++;
+            }
+            else if(curr_score > 0)
+            {
+                curr_score--;
+                power += tokens[j];
+                j--;
+            }
+            else
+                break;
+        } 
+        return max_score;
+    }
+};
